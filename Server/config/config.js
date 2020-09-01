6ed5a59d59a5ad37d4426cb2058762c533c82f3c
Haneum Kim <haneumkim@Haneumui-MacBookAir.local>
2020-09-01 11:53:46 +0900

    Add : added test setting for config.js
    config.js에 test를 위해 localhost로 테스트 환경을 추가했습니다

--- Server/config/config.js
+++ Server/config/config.js
@@ -8,4 +8,13 @@ module.exports = {
   dialect: 'mysql',
   logging: false,
   },
+  test: { // 테스트할 때 DATABASE 연결 정보
+    username: 'root',
+    password: process.env.DATABASE_PASSWORD,
+    database: 'KMC',
+    host: 'localhost',
+    port: 3306,
+    dialect: 'mysql',
+    logging: false,
+    },
 };
