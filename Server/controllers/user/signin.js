bae86abd9e8ab2a841a08277e42554f85952b740
Haneum Kim <haneumkim@Haneumui-MacBookAir.local>
2020-09-03 14:11:42 +0900

    Feat : Complete to create user Signin feature
    jwt 작동 확인 및 userSignin 기능을 만들었습니다

--- Server/controllers/user/signin.js
+++ Server/controllers/user/signin.js
@@ -1,5 +1,34 @@
+const db = require('../../models');
+const { jwtSign } = require('../../JWT');
+const { user } = db;
+
 module.exports = {
     post: (req, res) => {
-        res.end('ok!');
+        const { email, password } = req.body;
+
+        user
+        .findOne({
+            where: {
+                email,
+                password,
+            },
+        })
+        .then ((result) => {
+            if (!result) {
+                res.status(404).json({ message: 'invalid user' });
+            } else {
+                jwtSign(result.id, result.nickname, 'user').then((token) => {
+                    res.set('x-access-token', token);
+                    res.status(200).json({
+                        email: result.email,
+                        nickname: result.nickname,
+                        position: 'user',
+                    });
+                });
+            }
+        })
+        .catch((err)=>{
+            res.status(404).send(err);
+        });
     },
 };
