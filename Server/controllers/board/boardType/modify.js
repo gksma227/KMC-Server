c8b8ce8ca1e13fb83816c378d6dd2739a6a8ec1e
Haneum Kim <haneumkim@Haneumui-MacBookAir.local>
2020-09-16 15:52:16 +0900

    Feat : Create modify feature on board
    게시글 수정기능을 작성하였습니다

--- Server/controllers/board/boardType/modify.js
+++ Server/controllers/board/boardType/modify.js
@@ -1,5 +1,29 @@
+const db = require('../../../models')
+
+const { board } = db;
+
 module.exports = {
     patch: (req, res) => {
-        res.send(200);
+        const {
+            title, contents, register_date
+        } = req.body;
+        board
+        .findOne ({
+            where : {
+                id: req.params.id,
+            },
+        })
+        .then((result) => {
+            if (result) {
+                board.update({
+                    title, contents, register_date
+                }).then(() => res.sendStatus(200))
+            } else {
+                res.sendStatus(409)
+            }
+        })
+        .catch((err) => {
+            res.sendStatus(500)
+        })
     },
 };
