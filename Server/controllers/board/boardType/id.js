774a63dc48e69e764a2612b4d94f9544f344094e
Haneum Kim <haneumkim@Haneumui-MacBookAir.local>
2020-09-16 15:54:07 +0900

    Feat : Create id feature on board
    게시글을 클릭했을때 기능

--- Server/controllers/board/boardType/id.js
+++ Server/controllers/board/boardType/id.js
@@ -1,6 +1,28 @@
+const db = require('../../../models');
+const { board } = db;
+
 module.exports = {
     get: (req, res) => {
-        console.log('check: ', req.params);
-        res.send(200);
+        // console.log('check: ', req.params);
+        // res.send(200);
+        if (req.params.id){
+            board
+            .findOne({
+                where: {
+                    id: req.params.id,
+                },
+            })
+            .then((result)=>{
+                if (result){
+                    res.sendStatus(200)
+                } else {
+                    res.sendStatus(409)
+                }
+            })
+            .catch((err) => {
+                res.sendStatus(500)
+                console.log(err)
+            })
+        }
     },
 };
