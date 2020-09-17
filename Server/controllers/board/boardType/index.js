663d9aa40977774a268367041117bcfd58424fd8
Haneum Kim <haneumkim@Haneumui-MacBookAir.local>
2020-09-17 13:54:12 +0900

    Feat : Create index feature on board
    board의 목록을 보여주는 기능입니다

--- Server/controllers/board/boardType/index.js
+++ Server/controllers/board/boardType/index.js
@@ -1,6 +1,28 @@
+const db = require('../../../models');
+
+const { board } = db;
+
 module.exports = {
     get: (req, res) => {
-        res.send(200);
+        if (req.params.id){
+            board
+            .findAll({
+                where: {
+                    id: req.params.id
+                },
+            })
+            .then((result)=> {
+                if(result) {
+                    res.sendStatus(200)
+                } else {
+                    res.sendStatus(409)
+                }
+            })
+            .catch((err) => {
+                res.status(500)
+                console.log(err)
+            })
+        }
     },
     write: require('./write'),
     id: require('./id'),
