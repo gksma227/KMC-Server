7983b62925963e6dfe9ae5462f26d391d95e825f
Haneum Kim <haneumkim@Haneumui-MacBookAir.local>
2020-09-22 04:21:02 +0900

    eslint

--- Server/controllers/board/boardType/write.js
+++ Server/controllers/board/boardType/write.js
@@ -1,4 +1,5 @@
-const db = require('../../../models')
+const db = require('../../../models');
+
 const { board } = db;
 
 module.exports = {
@@ -6,26 +7,26 @@ module.exports = {
         // console.log('check: ', req.params);
         // res.send(201);
         const post = new board({
-            title : req.body.title,
-            userid : req.body.userid,
-            contents : req.body.contents,
-            register_date : new Date(),
-            views : req.body.views,
-            recommend : req.body.recommend,
-            boardtype_id : req.body.boardtype_id
+            title: req.body.title,
+            userid: req.body.userid,
+            contents: req.body.contents,
+            register_date: new Date(),
+            views: req.body.views,
+            recommend: req.body.recommend,
+            boardtype_id: req.body.boardtype_id,
         });
         post.save()
         .then((result) => {
            // console.log(result.dataValues);
             if (result) {
-               res.sendStatus(201)
+               res.sendStatus(201);
             } else {
                 res.sendStatus(409);
             }
         })
         .catch((err) => {
-            console.log(err)
+            console.log(err);
             res.sendStatus(500);
         });
-    }
-}
+    },
+};
--- Server/models/user.js
+++ Server/models/user.js
@@ -21,14 +21,15 @@ module.exports = (sequelize, DataTypes) => {
         password: {
             type: DataTypes.STRING(255),
             allowNull: true,
-        }
-    }, 
+        },
+    },
     {
         hooks: {
           // 비밀번호를 해싱해서 데이터베이스에 저장한다.
           beforeCreate: (data) => {
             const shasum = crypto.createHmac('sha512', salt);
             shasum.update(data.password);
+            // eslint-disable-next-line no-param-reassign
             data.password = shasum.digest('hex');
           },
           // 데이터를 찾기 전에 들어온 데이터의 password를 해싱함수를 통해 검증할 수 있게 한다.
@@ -36,6 +37,7 @@ module.exports = (sequelize, DataTypes) => {
             if (data.where.password) {
               const shasum = crypto.createHmac('sha512', salt);
               shasum.update(data.where.password);
+              // eslint-disable-next-line no-param-reassign
               data.where.password = shasum.digest('hex');
             }
           },
