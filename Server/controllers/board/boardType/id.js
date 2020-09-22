c897348fc4b29e8f34bf6034740aa553317403fc
Haneum Kim <haneumkim@Haneumui-MacBookAir.local>
2020-09-22 15:16:26 +0900

    merging

--- Server/controllers/board/boardType/id.js
+++ Server/controllers/board/boardType/id.js
@@ -1,33 +1,34 @@
 const db = require('../../../models');
+
 const { board } = db;
 const { user } = db;
 module.exports = {
     get: (req, res) => {
         // console.log('check: ', req.params);
         // res.send(200);
-        if (req.params.id){
+        if (req.params.id) {
             board
             .findOne({
                 include: [
                     {
                         model: user,
-                    }
+                    },
                 ],
                 where: {
-                    id: req.params.id
-                }
+                    id: req.params.id,
+                },
             })
-            .then((result)=>{
-                if (result){
-                    res.sendStatus(200)
+            .then((result) => {
+                if (result) {
+                    res.sendStatus(200);
                 } else {
-                    res.sendStatus(409)
+                    res.sendStatus(409);
                 }
             })
             .catch((err) => {
-                res.sendStatus(500)
-                console.log(err)
-            })
+                res.sendStatus(500);
+                console.log(err);
+            });
         }
     },
 };
