e30f7aaede584b1be48204a083054a6730ef482c
Haneum Kim <haneumkim@Haneumui-MacBookAir.local>
2020-09-22 15:11:33 +0900

    lint

--- Server/controllers/board/boardType/modify.js
+++ Server/controllers/board/boardType/modify.js
@@ -1,29 +1,30 @@
-const db = require('../../../models')
+const db = require('../../../models');
 
 const { board } = db;
 
 module.exports = {
     patch: (req, res) => {
         const {
-            title, contents, register_date
+            title, contents, register_date,
         } = req.body;
         board
-        .findOne ({
-            where : {
+        .findOne({
+            where: {
                 id: req.params.id,
             },
         })
         .then((result) => {
             if (result) {
                 board.update({
-                    title, contents, register_date
-                }).then(() => res.sendStatus(200))
+                    title, contents, register_date,
+                }).then(() => res.sendStatus(200));
             } else {
-                res.sendStatus(409)
+                res.sendStatus(409);
             }
         })
         .catch((err) => {
-            res.sendStatus(500)
-        })
+            res.sendStatus(500);
+            console.log(err);
+        });
     },
 };
